181996fc6d0ea7ea30b6cef1297974a0d89fa856
GIRISH CHANDRAN <girishchandran@GIRISHs-MacBook-Air.local>
2025-03-19 08:21:21 +0000

    Search Funtion added

--- client/src/components/Banner.jsx
+++ client/src/components/Banner.jsx
@@ -1,11 +1,32 @@
 import React from 'react'
 import { assets } from '../assets/assets'
 import Button from 'react-bootstrap/Button';
+import { AppContext } from '../context/AppContext';
+import { useContext, useRef } from 'react';
 
 const Banner = () => {
+
+        const {setSearchFilter, setIsSearched} = useContext(AppContext)
+
+        const titleRef =useRef(null)
+        const locationRef =useRef(null)
+
+        const onSearch = () => {
+            setSearchFilter ({
+                title:titleRef.current.value,
+                location: locationRef.current.value
+            })
+            setIsSearched(true)
+            console.log({
+                title:titleRef.current.value,
+                location: locationRef.current.value
+            });
+            
+        }
+
     return (
-        <div className='container 2x1:px-20 mx-auto my-10 bg-blue-100 py-10  md:px-20 text-center'>
-            <div className='max-w-2xl mx-auto'>
+        <div className='container 2x1:px-20 mx-auto my-10  py-10  md:px-20 text-center'>
+            <div className='container 2x1:px-20 mx-auto my-10 bg-blue-100 py-10  md:px-20 text-center'>
                 <h2 className='text-3xl md:text-4xl font-semibold text-gray-800'>Get Your First Job</h2>
                 <p className='text-gray-600 mt-3 text-lg'>
                     Over 10,000 jobs to apply. Take the first step towards your dream career.
@@ -16,6 +37,7 @@ const Banner = () => {
                     <div className="flex items-center bg-white shadow-md rounded-full overflow-hidden border border-gray-300 px-4">
                         <img src={assets.search_icon} alt="Search Icon" className="w-5 h-5 opacity-70" />
                         <input
+                        ref ={titleRef}
                             type="text"
                             placeholder="Search for jobs..."
                             className="p-2 flex-1 outline-none text-gray-700 bg-transparent placeholder-gray-500"
@@ -26,6 +48,7 @@ const Banner = () => {
                     <div className="flex items-center bg-white shadow-md rounded-full overflow-hidden border border-gray-300 px-4">
                         <img src={assets.location_icon} alt="Location Icon" className="w-5 h-5 opacity-70" />
                         <input
+                         ref ={locationRef}
                             type="text"
                             placeholder="Location"
                             className="p-2 flex-1 outline-none text-gray-700 bg-transparent placeholder-gray-500"
@@ -33,11 +56,22 @@ const Banner = () => {
                     </div>
 
                     {/* Search Button */}
-                    <Button className="">
+                    <Button onClick={onSearch} className="">
                         Search
                     </Button>
                 </div>
 
+            </div>
+            <div className='border border-grey-300 shadow-md mx-2 mt-5 p-6 rounded-md flex'>
+                <div className='flex justify-center gap-10 lg:gap-16 flex-wrap'>
+                    <p className='font-medium'>Trusted by</p>
+                    <img  className='h-6' src={assets.accenture_logo} alt="" />
+                    <img  className='h-6' src={assets.walmart_logo} alt="" />
+                    <img  className='h-6' src={assets.amazon_logo} alt="" />
+                    <img  className='h-6' src={assets.microsoft_logo} alt="" />
+                    <img  className='h-6' src={assets.samsung_logo} alt="" />
+                    <img  className='h-6' src={assets.adobe_logo} alt="" />
+                </div>
             </div>
         </div>
     )
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Banner from '../components/Banner'
+import JobListing from '../components/JobListing'
 
 function Home() {
   return (
     <div>
       <Navbar/>
       <Banner/>
+      <JobListing/>
     </div>
   )
 }
